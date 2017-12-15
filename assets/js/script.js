df5e70ac3ed99310bba043bf21b0fa1e6e1b94e1
ashwinkshenoy <ashwinkshenoy@gmail.com>
2017-12-15 16:44:45 +0530

    Images update

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 	// You can also add the html content in html page and still it will work!
 	var mybot = '<div class="chatCont" id="chatCont">'+
 								'<div class="bot_profile">'+
-									'<img src="assets/img/bot.png" class="bot_p_img">'+
+									'<img src="assets/img/bot2.svg" class="bot_p_img">'+
 									'<div class="close">'+
 										'<i class="fa fa-times" aria-hidden="true"></i>'+
 									'</div>'+
@@ -27,7 +27,7 @@ $(document).ready(function() {
 							'<div class="profile_div">'+
 								'<div class="row">'+
 									'<div class="col-hgt">'+
-										'<img src="assets/img/bot.png" class="img-circle img-profile">'+
+										'<img src="assets/img/bot2.svg" class="img-circle img-profile">'+
 									'</div><!--col-hgt end-->'+
 									'<div class="col-hgt">'+
 										'<div class="chat-txt">'+
